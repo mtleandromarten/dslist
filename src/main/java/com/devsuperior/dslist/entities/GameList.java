ddabd5e0a8e520cdd428446a4744a9e5db879196
mtleandromarten <leandro.marten@meetime.com.br>
2024-11-15 07:33:48 -0300

    implement domain model

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -1,8 +1,15 @@
 package com.devsuperior.dslist.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_game_list")
 public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
